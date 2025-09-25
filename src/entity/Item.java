782b353dfbe8d72c87ca1247c507fa0d6d604440
asimov-hy <asimov@hanyang.ac.kr>
2025-09-25 23:29:33 +0900

    feature: Added engine/ItemManager and entity/Item file
    Summary: Set base files for ItemManager engine codebase and Item entity codebase
    
    [+src/engine/ItemManager]
    Added ItemManager file to the engine folder to base future code relating to the management of item entitiess(spawning and effects) in the game. File is currently empty and no dependencies have been changed.
    
    [+src/entity/Item]
    Added Item file to the entity folder to base future code relating to the Item entities behavior in the game. File is currently a clone of the entity/bullet file as it will have many similar properties to said entity. No dependencies have been changed.

---
+++ src/entity/Item.java
@@ -0,0 +1,74 @@
+package entity;
+
+import java.awt.Color;
+
+import engine.DrawManager.SpriteType;
+
+/**
+ * Implements a Item that moves vertically up or down.
+ *
+ * @author <a href="mailto:RobertoIA1987@gmail.com">Roberto Izquierdo Amo</a>
+ *
+ */
+public class Item extends Entity {
+
+    /**
+     * Speed of the Item, positive or negative depending on direction -
+     * positive is down.
+     */
+    private int speed;
+
+    /**
+     * Constructor, establishes the Item's properties.
+     *
+     * @param positionX
+     *            Initial position of the Item in the X axis.
+     * @param positionY
+     *            Initial position of the Item in the Y axis.
+     * @param speed
+     *            Speed of the Item, positive or negative depending on
+     *            direction - positive is down.
+     */
+    public Item(final int positionX, final int positionY, final int speed) {
+        super(positionX, positionY, 3 * 2, 5 * 2, Color.WHITE);
+
+        this.speed = speed;
+        setSprite();
+    }
+
+    /**
+     * Sets correct sprite for the Item, based on speed.
+     */
+    public final void setSprite() {
+        if (speed < 0)
+            this.spriteType = SpriteType.Item;
+        else
+            this.spriteType = SpriteType.Item;
+    }
+
+    /**
+     * Updates the Item's position.
+     */
+    public final void update() {
+        this.positionY += this.speed;
+    }
+
+    /**
+     * Setter of the speed of the Item.
+     *
+     * @param speed
+     *            New speed of the Item.
+     */
+    public final void setSpeed(final int speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Getter for the speed of the Item.
+     *
+     * @return Speed of the Item.
+     */
+    public final int getSpeed() {
+        return this.speed;
+    }
+}
