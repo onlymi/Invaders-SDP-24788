a83027ca6f00d9272a8fc9a2533ea129b6a5270a
joinjun001 <109087027+Joinjun001@users.noreply.github.com>
2025-10-03 22:31:44 +0900

    feature: Implement HealthUp item feature
    Summary:
      1) Added HealthUp.java class as a subclass of Item.
      2) Implemented the applyEffect method to increase the player's life by 1 upon pickup.
      3) Updated the ItemType enum in Item.java to include the new HEALTH_UP type.
    
    [+entity/item/HealthUp.java]
    A new item class that grants the player one additional life when picked up.
    It overrides the applyEffect method to call the addLife function in GameState.

---
+++ src/entity/item/HealthUp.java
@@ -0,0 +1,21 @@
+package entity.item;
+
+
+import engine.GameState;
+import entity.Item;
+import engine.DrawManager.SpriteType;
+
+public class HealthUp extends Item{
+    public HealthUp(final int positionX, final int positionY) {
+        super(positionX, positionY, 2, SpriteType.ItemDefault, ItemType.ITEM_1, 10, 0, 0.5);
+    }
+
+    /**
+
+     Apply the item effect that increases coins.*
+     @param playerId
+     the ID of the player receiving the effect.*/
+    public void applyEffect(final GameState gameState, final int playerId) {
+        if (gameState == null) return;
+        gameState.addLife(playerId, this.effectValue);}
+}
