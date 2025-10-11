1fdb6f29dce44a56eaed6d7b7ad7f667ae13c427
PkHamin <jqkrgkals123@gmail.com>
2025-10-11 23:07:32 +0900

    feat: change Lives to heart shape

--- src/engine/DrawManager.java
+++ src/engine/DrawManager.java
@@ -76,7 +76,9 @@ public final class DrawManager {
 		/** Bonus ship. */
 		EnemyShipSpecial,
 		/** Destroyed enemy ship. */
-		Explosion
+		Explosion,
+        /** Heart for lives display. */
+        Heart
 	};
 
 	/**
@@ -102,6 +104,7 @@ public final class DrawManager {
 			spriteMap.put(SpriteType.EnemyShipC2, new boolean[12][8]);
 			spriteMap.put(SpriteType.EnemyShipSpecial, new boolean[16][7]);
 			spriteMap.put(SpriteType.Explosion, new boolean[13][7]);
+            spriteMap.put(SpriteType.Heart, new boolean[13][12]); //추가
 
 			fileManager.loadSprite(spriteMap);
 			logger.info("Finished loading the sprites.");
@@ -279,9 +282,11 @@ public final class DrawManager {
 		backBufferGraphics.setFont(fontRegular);
 		backBufferGraphics.setColor(Color.WHITE);
 		backBufferGraphics.drawString(Integer.toString(lives), 20, 25);
-		Ship dummyShip = new Ship(0, 0);
+		Entity heart = new Entity(0,0,5*2, 4*2,Color.RED) {
+            {this.spriteType = SpriteType.Heart;}
+        };
 		for (int i = 0; i < lives; i++)
-			drawEntity(dummyShip, 40 + 35 * i, 10);
+			drawEntity(heart, 40 + 35 * i, 9);
 	}
 
 	/**
