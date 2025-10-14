5eba6285236cb07d371828c572f8a5d3f6a1ee96
yutak <yutak3882@gmail.com>
2025-10-14 22:34:05 +0900

    Add:Create a return button in the upper left corner (mouse-click function)

--- src/screen/AchievementScreen.java
+++ src/screen/AchievementScreen.java
@@ -21,11 +21,33 @@ public class AchievementScreen extends Screen {
             this.returnCode = 1;
             this.isRunning = false;
         }
+
+        // back button click event
+        if (inputManager.isMouseClicked()) {
+            int mx = inputManager.getMouseX();
+            int my = inputManager.getMouseY();
+            java.awt.Rectangle backBox = drawManager.getBackButtonHitbox(this);
+
+            if (backBox.contains(mx, my)) {
+                this.returnCode = 1;
+                this.isRunning = false;
+            }
+        }
     }
 
     private void draw() {
         drawManager.initDrawing(this);
         drawManager.drawAchievementMenu(this);
+
+        // hover highlight
+        int mx = inputManager.getMouseX();
+        int my = inputManager.getMouseY();
+        java.awt.Rectangle backBox = drawManager.getBackButtonHitbox(this);
+
+        if (backBox.contains(mx, my)) {
+            drawManager.drawBackButton(this, true);
+        }
+
         drawManager.completeDrawing(this);
     }
 }
