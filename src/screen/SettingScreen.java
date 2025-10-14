e588f7075409671ceedd65d0df3b2995d17cc5ee
KDW <kkjmo64@gmail.com>
2025-10-14 22:54:35 +0900

    feat(screen): Add basic structure and navigation for SettingScreen
    Implements the foundational logic for the new settings screen.
    
    - Create the new `SettingScreen.java` class.
    - Add menu items ("Volume", "1P Keyset", "2P Keyset") and a state variable (`selectedMenuItem`) to track the current selection.
    - Implement input handling in `update()` to allow UP/DOWN navigation through the menu items.
    - The `draw()` method now calls `drawSettingLayout` to render the screen and includes placeholder `if` blocks for future content panels.

--- src/screen/SettingScreen.java
+++ src/screen/SettingScreen.java
@@ -1,6 +1,15 @@
 package screen;
 
+import engine.Core;
+
+import java.awt.event.KeyEvent;
+
 public class SettingScreen extends Screen {
+    private static final int volumeMenu = 0;
+    private static final int firstplayerMenu = 1;
+    private static final int secondplayerMenu= 2;
+    private final String[] menuItem = {"Volume", "1P Keyset", "2P Keyset"};
+    private int selectMenuItem;
 
     /**
      * Constructor, establishes the properties of the screen.
@@ -14,7 +23,6 @@ public class SettingScreen extends Screen {
 
         this.returnCode = 1;
     }
-
     /**
      * Starts the action.
      *
@@ -25,16 +33,31 @@ public class SettingScreen extends Screen {
 
         return this.returnCode;
     }
+    public final void initialize(){
+        super.initialize();
+        this.inputDelay = Core.getCooldown(200);
+        this.inputDelay.reset();
+        this.selectMenuItem = volumeMenu;
+    }
 
     /**
      * Updates the elements on screen and checks for events.
      */
     protected final void update() {
         super.update();
-        draw();
-        if (inputManager.isKeyDown(java.awt.event.KeyEvent.VK_SPACE)
+
+        if(inputManager.isKeyDown(KeyEvent.VK_UP)&&this.inputDelay.checkFinished()&&this.selectMenuItem>0) {
+            this.selectMenuItem--;
+            this.inputDelay.reset();
+        }
+        if(inputManager.isKeyDown(KeyEvent.VK_DOWN)&&this.inputDelay.checkFinished()&&this.selectMenuItem<menuItem.length-1) {
+            this.selectMenuItem++;
+            this.inputDelay.reset();
+        }
+        if (inputManager.isKeyDown(KeyEvent.VK_SPACE)
                 && this.inputDelay.checkFinished())
             this.isRunning = false;
+        draw();
     }
 
     /**
@@ -42,7 +65,18 @@ public class SettingScreen extends Screen {
      */
     private void draw() {
         drawManager.initDrawing(this);
-
+        drawManager.drawSettingLayout(this, menuItem,this.selectMenuItem);
+        switch(this.selectMenuItem) {
+            case volumeMenu:
+                //function
+                break;
+            case firstplayerMenu:
+                //function
+                break;
+            case secondplayerMenu:
+                //function
+                break;
+        }
         drawManager.drawSettingMenu(this);
         drawManager.completeDrawing(this);
     }
